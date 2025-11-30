a3b6f556cd979005cf43f2cd7f0d567b31735a17
Neetesh26 <neeteshprajapati009@gmail.com>
2025-12-01 00:32:41 +0530

    Refactor FileHandler.js to enhance file operations and add example_copy1.txt with initial content

--- Files-modules/FileHandler.js
+++ Files-modules/FileHandler.js
@@ -12,3 +12,26 @@ console.log('File content:', data);
 // append file ------>
 fs.appendFileSync("example.txt", " this is appended text\n", 'utf-8');
 console.log('File appended successfully');
+
+// read file again to see appended content
+const updatedData = fs.readFileSync('example.txt', 'utf-8');
+console.log('Updated File content:', updatedData);
+
+// copy file ------>
+fs.copyFileSync('example.txt' , 'example_copy1.txt');
+console.log('File copied successfully');
+
+fs.copyFileSync('example.txt' , 'example_copy2.txt');
+console.log('File copied successfully');
+
+// delete file ------>
+fs.unlinkSync('example_copy2.txt');
+console.log('File deleted successfully');
+
+// copy file again ------>
+// fs.copyFileSync('example.txt' , 'example_copy2.txt');
+// console.log('File copied successfully');
+
+// rename file ------>
+// fs.renameSync('example_copy1.txt', 'renamed_example_copy2.txt');
+// console.log('File renamed successfully');
