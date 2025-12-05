3099e09d37cb682c448d10c9a96f9a25349e0bf7
Neetesh26 <neeteshprajapati009@gmail.com>
2025-12-05 23:04:02 +0530

    Update .gitignore, enhance logging, and add URL parsing in server

--- log-Files/index.js
+++ log-Files/index.js
@@ -1,20 +1,26 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 const myServer = http.createServer(
     (req, res) => {
 
         if (req.url === '/favicon.ico') { return res.end(); }
-
+        const parsedUrl = url.parse(req.url, true);
+        console.log(parsedUrl);
+        
         const logEntry = `Request received at : ${new Date().toString()} for URL : ${req.url}\n`;
         fs.appendFile('server_log.txt', logEntry, (err, data) => {
 
-            switch (req.url) {
+            switch (parsedUrl.pathname) {
                 case '/':
-                    res.end('Welcome to the Home Page!\n');
+                    res.end(`Welcome ${parsedUrl.query.name}!\n`);
                     break;
                 case '/about':
                     res.end('This is the About Page.\n');
+                    break;
+                    case '/search':
+                    res.end(`Search results for: ${parsedUrl.query}\n`);
                     break;
                 default:
                     res.end('404 Not Found\n');
