32baa4cc9f44ab4e85ec7201649ffb007aff3eec
Neetesh26 <neeteshprajapati009@gmail.com>
2025-12-13 23:58:57 +0530

    fix: update MongoDB connection string and improve home view for URL display

--- UrlShortner/routes/staticRoute.routes.js
+++ UrlShortner/routes/staticRoute.routes.js
@@ -6,4 +6,8 @@ router.get('/', async (req, res) => {
     const allUrls = await urlModel.find({})
    return  res.render('home', { allUrls });
 })  
+router.get('/url', async (req, res) => {
+    const allUrls = await urlModel.find({})
+   return  res.render('home', { allUrls });
+})  
 module.exports = router
